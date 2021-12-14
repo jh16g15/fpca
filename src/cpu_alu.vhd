81e2d87941729a3659da052ab409d2346c3ff8c8
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2021-12-14 22:28:57 +0000

    massively overcomplicated update to ALU logic for main ops

--- src/cpu_alu.vhd
+++ src/cpu_alu.vhd
@@ -4,10 +4,6 @@ use ieee.numeric_std.all;
 
 use work.riscv_instructions_pkg.all;
 
--- types of Jump instructions in RV32I:
-
--- Unconditional
-
 entity cpu_alu is
   -- generic (
 
@@ -47,49 +43,116 @@ architecture rtl of cpu_alu is
   signal alu_A : std_logic_vector(31 downto 0);
   signal alu_B : std_logic_vector(31 downto 0);
 
-  type t_alu_op is (ADD_SUB_op, SLL_op, SLT_op, SLTU_op, XOR_op, SRL_op, SRA_op, OR_op, AND_op);
+  type t_alu_op is (ADD_SUB_op, SLL_op, SLT_op, SLTU_op, XOR_op, SRL_op, SRA_op, OR_op, AND_op, NULL_op);
   signal operation_type : t_alu_op;
+
+  signal sub_select : std_logic;
 begin
 
   -- alu input muxes
   alu_A <= rs1 when alu_A_sel = ALU_A_RS1 else pc;
   alu_B <= rs2 when alu_B_sel = ALU_B_RS2 else imm;
 
-  -- alu_comb : process(all) is
-  -- begin
-  --   -- default outputs 
-  --   branch_en <= '0';
-  --   alu_out <= x"DEADBEEF";
-  --   case(opcode) is 
-  --     when OPCODE_LUI => 
-  --       -- 
-  --     when OPCODE_AUIPC => 
-      
-  --     when OPCODE_JAL => 
-      
-  --     when OPCODE_JALR => 
 
-  --     when OPCODE_BRANCH => 
-  --       case(funct3) is 
-  --         when BRANCH_BEQ_FUNC3 => 
-  --         when BRANCH_BNE_FUNC3 => 
-  --         when BRANCH_BLT_FUNC3  => 
-  --         when BRANCH_BGE_FUNC3  => 
-  --         when BRANCH_BLTU_FUNC3 => 
-  --         when BRANCH_BGEU_FUNC3 => 
-  --         when others => 
-  --       end case;
-  --     when OPCODE_LOAD => 
-
-  --     when OPCODE_STORE =>
-
-  --     when OPCODE_OP =>
+  alu_input_select : process(all) is
+  begin
+      -- defaults
+      alu_A_sel <= ALU_A_RS1;
+      alu_B_sel <= ALU_B_RS2;
+      operation_type <= NULL_op;
+      sub_select <= '0';
+      case(opcode) is
+        when OPCODE_LUI => 
+          alu_A_sel <= ALU_A_RS1; -- make sure this is 'x0' in decode stage - consider bypass
+          alu_B_sel <= ALU_B_IMM;
+          operation_type <= ADD_SUB_op;
+          sub_select <= '0';
+        when OPCODE_AUIPC => -- PC+Imm
+          alu_A_sel <= ALU_A_PC;
+          alu_B_sel <= ALU_B_IMM;
+          operation_type <= ADD_SUB_op;
+          sub_select <= '0';
+        when OPCODE_JAL => -- PC+Imm, handle in Branch Target Adder
+          operation_type <= NULL_op;
         
-  --     when OPCODE_OP_IMM => 
-      
-  --   end case;
-  -- end process;
+        when OPCODE_JALR => -- RS1 + Imm,  then send result to branch target adder
+          alu_A_sel <= ALU_A_PC;
+          alu_B_sel <= ALU_B_IMM;
+          operation_type <= ADD_SUB_op;
+          sub_select <= '0';
+        when OPCODE_BRANCH => -- handle in branch condition checker (could use SLT(U) here to reduce logic)
+          operation_type <= NULL_op;
+        when OPCODE_LOAD => -- address to fetch from = rs1+imm
+          alu_A_sel <= ALU_A_RS1;
+          alu_B_sel <= ALU_B_IMM;
+          operation_type <= ADD_SUB_op;
+          sub_select <= '0';
+        when OPCODE_STORE => -- address to write to = rs1+imm
+          alu_A_sel <= ALU_A_RS1;
+          alu_B_sel <= ALU_B_IMM;
+          operation_type <= ADD_SUB_op;
+          sub_select <= '0';
+        when OPCODE_OP | OPCODE_OP_IMM => -- register-register or register-immmediate arithmetic 
+          alu_A_sel <= ALU_A_RS1;
+          alu_B_sel <= ALU_B_RS2 when opcode = OPCODE_OP else ALU_B_IMM;
+          case(funct3) is 
+            when OP_ADD_FUNC3 | OP_SUB_FUNC3 => 
+              operation_type <= ADD_SUB_op;
+              sub_select <= funct7(5) when opcode = OPCODE_OP else '0'; -- OP_IMM is addition only
+            when OP_SLT_FUNC3  => 
+              operation_type <= SLT_op;
+            when OP_SLTU_FUNC3 => 
+              operation_type <= SLTU_op;
+            when OP_XOR_FUNC3  => 
+              operation_type <= XOR_op;
+            when OP_OR_FUNC3   => 
+              operation_type <= OR_op;
+            when OP_AND_FUNC3  => 
+              operation_type <= AND_op;
+            when OP_SLL_FUNC3  => 
+              operation_type <= SLL_op;
+            when OP_SRL_FUNC3 | OP_SRA_FUNC3 =>  
+              if funct7(5) = '1' then
+                operation_type <= SRA_op;
+              else
+                operation_type <= SRL_op;
+              end if;
+            when others => 
+              operation_type <= NULL_op;
+          end case;
+      end case;
     
+  end process;
+  
+    
+
+    branch_condition_checker : process (all) is
+
+    begin
+      -- default outputs 
+      branch_en <= '0';
+      
+      case (opcode) is
+        when OPCODE_JAL | OPCODE_JALR => 
+          branch_en <= '1';
+        when OPCODE_BRANCH => 
+          case(funct3) is 
+            when BRANCH_BEQ_FUNC3 => 
+            
+            when BRANCH_BNE_FUNC3 => 
+            when BRANCH_BLT_FUNC3  => 
+            when BRANCH_BGE_FUNC3  => 
+            when BRANCH_BLTU_FUNC3 => 
+            when BRANCH_BGEU_FUNC3 => 
+            when others => 
+          end case;
+        end case;
+
+    end process branch_unit;
+
+
+
+
 
     alu_op_select_comb : process (all) is
       
@@ -111,15 +174,16 @@ begin
       variable shamt : unsigned(4 downto 0);
       variable shamt_int : natural range 0 to 31;
 
-      variable sub_select : std_logic;
+      
     begin
       -- set up the intermediate variables
-      sub_select := funct7(5);
+      
       alu_input_A_u := unsigned(alu_input_A);
       alu_input_B_u := unsigned(alu_input_B);
       alu_input_A_s := signed(alu_input_A);
       alu_input_B_s := signed(alu_input_B);
 
+      shamt := alu_input_B_u(4 downto 0); -- 5 bits (max 31)
       shamt_int := to_integer(shamt);
 
       case(operation_type) is 
@@ -155,9 +219,11 @@ begin
         when AND_op => 
           alu_output := alu_input_A and alu_input_B;
         when others =>
-          null;
+          alu_output := x"DEADBEEF";
       end case;
-
+      
+      alu_out <= alu_output;
+      
     end process alu_op_select_comb;
 
 
