3abdbd5875bffc9ee7dcfa413a8b191d6c252ec5
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2021-12-31 15:14:31 +0000

    clean up constants

--- hdl/src/packages/riscv_instructions_pkg.vhd
+++ hdl/src/packages/riscv_instructions_pkg.vhd
@@ -6,12 +6,12 @@ use work.joe_common_pkg.all;
 
 package riscv_instructions_pkg is
 
-  constant INSTR_ADDR_W : integer := 32;
+  constant INSTR_W : integer := 32;
   constant REG_ADDR_W : integer := 5;
-  constant IMM_ADDR_W : integer := 32;
-  constant OPCODE_ADDR_W : integer := 7;
-  constant FUNCT7_ADDR_W : integer := 7;
-  constant FUNCT3_ADDR_W : integer := 3;
+  constant IMM_W : integer := 32;
+  constant OPCODE_W : integer := 7;
+  constant FUNCT7_W : integer := 7;
+  constant FUNCT3_W : integer := 3;
 
 
   -- type t_control_sigs is record
@@ -39,12 +39,12 @@ package riscv_instructions_pkg is
 
   --! Assembles a 32 bit instruction from its arguments
   function f_build_instr(
-    opcode : std_logic_vector(OPCODE_ADDR_W-1 downto 0) := (others => '0');
+    opcode : std_logic_vector(OPCODE_W-1 downto 0) := (others => '0');
     rs1 : std_logic_vector(REG_ADDR_W-1 downto 0) := (others => '0');
     rs2 : std_logic_vector(REG_ADDR_W-1 downto 0) := (others => '0');
     rd  : std_logic_vector(REG_ADDR_W-1 downto 0) := (others => '0');
-    funct3 : std_logic_vector(FUNCT3_ADDR_W-1 downto 0) := (others => '0');
-    funct7 : std_logic_vector(FUNCT7_ADDR_W-1 downto 0) := (others => '0');
+    funct3 : std_logic_vector(FUNCT3_W-1 downto 0) := (others => '0');
+    funct7 : std_logic_vector(FUNCT7_W-1 downto 0) := (others => '0');
     Imm12 : std_logic_vector(12-1 downto 0) := (others => '0');
     Imm20 : std_logic_vector(20-1 downto 0) := (others => '0')
     ) return std_logic_vector;
@@ -52,37 +52,37 @@ package riscv_instructions_pkg is
   -- Opcodes
   ------------------------------------
   -- most of these I am not implementing in RV32I
-  constant OPCODE_LOAD : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_000_11"; -- I
-  constant OPCODE_LOAD_FP : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_001_11"; -- Float
-  constant OPCODE_CUSTOM0 : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_010_11";
-  constant OPCODE_MISC_MEM : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_011_11"; -- I, FENCEs not implemented
-  constant OPCODE_OP_IMM : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_100_11"; -- I 
-  constant OPCODE_AUIPC : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_101_11"; -- I
-  constant OPCODE_OP_IMM_32 : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"00_110_11"; -- 64+?
+  constant OPCODE_LOAD : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_000_11"; -- I
+  constant OPCODE_LOAD_FP : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_001_11"; -- Float
+  constant OPCODE_CUSTOM0 : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_010_11";
+  constant OPCODE_MISC_MEM : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_011_11"; -- I, FENCEs not implemented
+  constant OPCODE_OP_IMM : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_100_11"; -- I 
+  constant OPCODE_AUIPC : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_101_11"; -- I
+  constant OPCODE_OP_IMM_32 : std_logic_vector(OPCODE_W - 1 downto 0) := b"00_110_11"; -- 64+?
 
-  constant OPCODE_STORE : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_000_11"; -- I
-  constant OPCODE_STORE_FP : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_001_11"; -- Float
-  constant OPCODE_CUSTOM1 : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_010_11";
-  constant OPCODE_AMO : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_011_11"; -- Atomic
-  constant OPCODE_OP : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_100_11"; -- I
-  constant OPCODE_LUI : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_101_11"; -- I
-  constant OPCODE_OP_32 : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"01_110_11"; -- 64+?
+  constant OPCODE_STORE : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_000_11"; -- I
+  constant OPCODE_STORE_FP : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_001_11"; -- Float
+  constant OPCODE_CUSTOM1 : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_010_11";
+  constant OPCODE_AMO : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_011_11"; -- Atomic
+  constant OPCODE_OP : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_100_11"; -- I
+  constant OPCODE_LUI : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_101_11"; -- I
+  constant OPCODE_OP_32 : std_logic_vector(OPCODE_W - 1 downto 0) := b"01_110_11"; -- 64+?
 
-  constant OPCODE_MADD : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"10_000_11"; -- Float
-  constant OPCODE_MSUB : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"10_001_11"; -- Float
-  constant OPCODE_NMSUB : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"10_010_11"; -- Float
-  constant OPCODE_NMADD : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"10_011_11"; -- Float
-  constant OPCODE_OP_FP : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"10_100_11"; -- Float
-  -- constant OPCODE_RESERVED : std_logic_vector(OPCODE_ADDR_W-1 downto 0) := b"10_101_11";
-  constant OPCODE_CUSTOM2 : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"10_110_11";
+  constant OPCODE_MADD : std_logic_vector(OPCODE_W - 1 downto 0) := b"10_000_11"; -- Float
+  constant OPCODE_MSUB : std_logic_vector(OPCODE_W - 1 downto 0) := b"10_001_11"; -- Float
+  constant OPCODE_NMSUB : std_logic_vector(OPCODE_W - 1 downto 0) := b"10_010_11"; -- Float
+  constant OPCODE_NMADD : std_logic_vector(OPCODE_W - 1 downto 0) := b"10_011_11"; -- Float
+  constant OPCODE_OP_FP : std_logic_vector(OPCODE_W - 1 downto 0) := b"10_100_11"; -- Float
+  -- constant OPCODE_RESERVED : std_logic_vector(OPCODE_W-1 downto 0) := b"10_101_11";
+  constant OPCODE_CUSTOM2 : std_logic_vector(OPCODE_W - 1 downto 0) := b"10_110_11";
 
-  constant OPCODE_BRANCH : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"11_000_11"; -- I
-  constant OPCODE_JALR : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"11_001_11"; -- I
-  -- constant OPCODE_RESERVED : std_logic_vector(OPCODE_ADDR_W-1 downto 0) := b"11_010_11";
-  constant OPCODE_JAL : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"11_011_11"; -- I
-  constant OPCODE_SYSTEM : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"11_100_11"; -- I
-  -- constant OPCODE_RESERVED : std_logic_vector(OPCODE_ADDR_W-1 downto 0) := b"11_101_11";
-  constant OPCODE_CUSTOM3 : std_logic_vector(OPCODE_ADDR_W - 1 downto 0) := b"11_110_11";
+  constant OPCODE_BRANCH : std_logic_vector(OPCODE_W - 1 downto 0) := b"11_000_11"; -- I
+  constant OPCODE_JALR : std_logic_vector(OPCODE_W - 1 downto 0) := b"11_001_11"; -- I
+  -- constant OPCODE_RESERVED : std_logic_vector(OPCODE_W-1 downto 0) := b"11_010_11";
+  constant OPCODE_JAL : std_logic_vector(OPCODE_W - 1 downto 0) := b"11_011_11"; -- I
+  constant OPCODE_SYSTEM : std_logic_vector(OPCODE_W - 1 downto 0) := b"11_100_11"; -- I
+  -- constant OPCODE_RESERVED : std_logic_vector(OPCODE_W-1 downto 0) := b"11_101_11";
+  constant OPCODE_CUSTOM3 : std_logic_vector(OPCODE_W - 1 downto 0) := b"11_110_11";
   -- Instruction Encoding Definitions:
   ----------------------------------------------
   constant ENC_TYPE_LOAD : t_encoding := I_type;
@@ -168,17 +168,17 @@ package body riscv_instructions_pkg is
 
   --! Assembles a 32 bit instruction from its arguments
   function f_build_instr(
-  opcode : std_logic_vector(OPCODE_ADDR_W-1 downto 0) := (others => '0');
+  opcode : std_logic_vector(OPCODE_W-1 downto 0) := (others => '0');
   rs1 : std_logic_vector(REG_ADDR_W-1 downto 0) := (others => '0');
   rs2 : std_logic_vector(REG_ADDR_W-1 downto 0) := (others => '0');
   rd : std_logic_vector(REG_ADDR_W-1 downto 0) := (others => '0');
-  funct3 : std_logic_vector(FUNCT3_ADDR_W-1 downto 0) := (others => '0');
-  funct7 : std_logic_vector(FUNCT7_ADDR_W-1 downto 0) := (others => '0');
+  funct3 : std_logic_vector(FUNCT3_W-1 downto 0) := (others => '0');
+  funct7 : std_logic_vector(FUNCT7_W-1 downto 0) := (others => '0');
   Imm12 : std_logic_vector(12-1 downto 0) := (others => '0');
   Imm20 : std_logic_vector(20-1 downto 0) := (others => '0')
   ) return std_logic_vector is 
     variable enc : t_encoding;
-    variable instr : std_logic_vector(INSTR_ADDR_W-1 downto 0) := (others => '0');
+    variable instr : std_logic_vector(INSTR_W-1 downto 0) := (others => '0');
   begin
     case (opcode) is
       when OPCODE_OP => enc := R_type;
