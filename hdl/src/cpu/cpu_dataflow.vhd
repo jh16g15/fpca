eddeacfb8911463b02362178fcbfb3c9852b4a11
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2021-12-31 15:15:20 +0000

    Gate the register writeback WE with the mem_rdata_valid if executing a LOAD

--- hdl/src/cpu/cpu_dataflow.vhd
+++ hdl/src/cpu/cpu_dataflow.vhd
@@ -7,6 +7,7 @@ use work.joe_common_pkg.all;
 use work.wb_pkg.all;
 
 --! Controls register writeback and Memory Loads/Stores
+--! TODO: handle mem_err
 entity cpu_dataflow is
     port (
         clk   : in std_logic;
@@ -16,19 +17,19 @@ entity cpu_dataflow is
         ret_addr_in   : in std_logic_vector(31 downto 0);
 
         rs2_data_in : in std_logic_vector(31 downto 0); --! store data
-
-        reg_write_data_out : out std_logic_vector(31 downto 0); --! Writeback Data
-
-        --for future pipelining
+        --for future pipelining/error handling
+        branch_en_in      : in std_logic;
+        branch_en_out     : out std_logic;
         branch_target_in  : in std_logic_vector(31 downto 0);
         branch_target_out : out std_logic_vector(31 downto 0);
-        write_reg_we_in   : in std_logic;
-        write_reg_we_out  : out std_logic;
-
         -- writeback Mux control signals (from decoder)
         write_load_in     : in std_logic;
         write_alu_in      : in std_logic;
         write_ret_addr_in : in std_logic;
+        uses_writeback_in : in std_logic;
+
+        write_reg_data_out : out std_logic_vector(31 downto 0); --! Writeback Data
+        write_reg_we_out   : out std_logic;
 
         -- MEM access status/control signals (from cpu_control)
         mem_req_in   : in std_logic;
@@ -58,33 +59,36 @@ architecture rtl of cpu_dataflow is
     signal mem_wdata : std_logic_vector(31 downto 0);
     signal mem_rdata : std_logic_vector(31 downto 0);
 begin
-    -- for future pipelining
+    -- for future pipelining/error handling
     branch_target_out <= branch_target_in;
-    write_reg_we_out  <= write_reg_we_in;
+    branch_en_out <= branch_en_in;
+
+    -- gate register writeback enable based on the MEM RDATA VALID signal
+    write_reg_we_out <= mem_done_out when write_load_in = '1' else uses_writeback_in;
 
     -- decode the LOAD/STORE funct3 field
     size          <= wb_get_transfer_size(func3_in); -- b8, b16, b32
     unsigned_flag <= func3_in(2);
 
     process (all)
-        variable   var_wb_addr : std_logic_vector(31 downto 0);
-        variable   var_wb_sel : std_logic_vector(3 downto 0);
+        variable var_wb_addr : std_logic_vector(31 downto 0);
+        variable var_wb_sel  : std_logic_vector(3 downto 0);
     begin
         -- calculate the Wishbone Select lines
         wb_byte_addr_to_byte_sel(alu_output_in, size, var_wb_addr, var_wb_sel);
         wb_addr <= var_wb_addr;
-        wb_sel <= var_wb_sel;
+        wb_sel  <= var_wb_sel;
 
         -- calculate the WDATA to send
         mem_wdata <= wb_align_store_data(rs2_data_in, wb_sel);
 
         -- writeback_mux
         if write_load_in = '1' then
-            reg_write_data_out <= mem_rdata;
+            write_reg_data_out <= mem_rdata;
         elsif write_ret_addr_in = '1' then
-            reg_write_data_out <= ret_addr_in;
+            write_reg_data_out <= ret_addr_in;
         else -- write ALU result
-            reg_write_data_out <= alu_output_in;
+            write_reg_data_out <= alu_output_in;
         end if;
     end process;
 
