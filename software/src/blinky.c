385c708b51dc5c3e6507dbc22808989281015927
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-01-03 18:39:53 +0000

    fixed stack pointer initial misalignment causing memory access issues

--- software/src/blinky.c
+++ software/src/blinky.c
@@ -6,31 +6,54 @@
 #define GPIO_BTN    (*((volatile unsigned long *) 0x10000100 ))
 #define GPIO_SW     (*((volatile unsigned long *) 0x10000104 ))
 
-#define DELAY 10000
+// #define DELAY 10000000  // 10 million
+#define DELAY 10  // 10 million
 
-// set up the Stack Pointer to x0000_1000
-asm("li sp, 0x00001FFF");
+// set up the Stack Pointer to x0000_1FFC (32-bit aligned)
+asm("li sp, 0x00001FFC");
 
 // function declarations
-// void main(void);
 // void delay(int dly);
 
+// // without functions
 void main(void){
-    int count = 0;
+    int count = 0x1000;
+    volatile int tmp = 0;
+    int i = 0;
     while (1)
     {
         GPIO_LED = 1;
-        // delay(DELAY);
+        for (i = 0; i < 10; ++i){
+            tmp = i;
+        }
         GPIO_LED = 0;
-        // delay(DELAY);
         count = count + 1;
         Q_SSEG = count;
+        for (i = 0; i < 10; i++){
+            tmp = i;
+        }
     }
 }
 
 
+
+// void main(void){
+//     int count = 0;
+//     while (1)
+//     {
+//         GPIO_LED = 1;
+//         delay(DELAY);
+//         GPIO_LED = 0;
+//         delay(DELAY);
+//         count = count + 1;
+//         Q_SSEG = count;
+//     }
+// }
+
+
 // // TODO: characterise this delay
 // void delay(int dly){
+//     asm("");
 //     int i = 0;
 //     while (i < dly){
 //         i++;
