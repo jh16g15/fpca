57107a9656a99d2c094428e956e816cf3d5fe8d2
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-01-04 00:58:57 +0000

    clean up build script

--- software/build_test.sh
+++ software/build_test.sh
@@ -1,13 +1,11 @@
 
-LINKER_SCRIPT="test.ld"
+LINKER_SCRIPT="riscv32-fpca.ld"
 
-# -c for compile-only, no linkng
 GCC_OPT="-O0"
-# old
-# GCC_ARGS="-g -march=rv32i -mabi=ilp32 -nostartfiles -nostdlib -nodefaultlibs"
 # from https://twilco.github.io/riscv-from-scratch/2019/04/27/riscv-from-scratch-2.html
-GCC_ARGS="-g -march=rv32i -mabi=ilp32 -ffreestanding -Wl,--gc-sections -nostartfiles -nostdlib -nodefaultlibs"
-LD_ARGS="-Wl,-T,riscv32-fpca.ld"
+GCC_ARGS="-g -march=rv32i -mabi=ilp32 -ffreestanding -Wl,--gc-sections -nostartfiles -nostdlib -nodefaultlibs"      # no other libraries
+# GCC_ARGS="-g -march=rv32i -mabi=ilp32 -ffreestanding -Wl,--gc-sections -nostartfiles "    # allow stdlib and defaultlibs (for soft mult/div etc)
+LD_ARGS="-Wl,-T,$LINKER_SCRIPT"
 
 
 GCC_INPUT="src/crt0.s src/blinky.c"
@@ -19,12 +17,11 @@ echo "Cleaning build/"
 rm -rf build/*
 
 echo "Building Test Software..."
-# riscv32-unknown-elf-gcc $GCC_OPT $GCC_ARGS $GCC_INPUT -o $GCC_OUTPUT
 riscv32-unknown-elf-gcc $GCC_OPT $GCC_ARGS $LD_ARGS $GCC_INPUT -o $GCC_OUTPUT
 riscv32-unknown-elf-objdump -d -S $GCC_OUTPUT
 
 echo "Creating HEX file..."
-## NOTE: we need to use the SiFive elf2hex (https://github.com/sifive/elf2hex), 
+## NOTE: we need to use the SiFive elf2hex (https://github.com/sifive/elf2hex),
 #        not the elf2hex packaged witth Spike
 riscv32-unknown-elf-elf2hex --bit-width 32 --input $GCC_OUTPUT --output $HEX_OUTPUT
 
