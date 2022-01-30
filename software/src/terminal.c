1403d74b20b54571ecb7da3a791f738f271f5f2c
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-01-30 01:23:09 +0000

    Adds teminal API to provide a hardware-independent text display buffer

--- software/src/terminal.c
+++ software/src/terminal.c
@@ -3,8 +3,34 @@
 #include "terminal.h"
 #include <stdlib.h>
 
+/*
+ * Private funcs
+ */
+inline int clamp_cursor_x(t_terminal *t)
+{
+    if (t->x >= t->w)
+    {
+        t->x = t->w - 1;
+    }
+}
+inline int clamp_cursor_y(t_terminal *t)
+{
+    if (t->y >= t->h)
+    {
+        t->y = t->h - 1;
+    }
+}
+// if x > w, set x=0 and increment y
+int wrap_cursor(t_terminal *t)
+{
+}
 
-t_terminal* create_terminal(int w, int h)
+/*
+ * Public funcs
+ */
+
+// allocate memory for a new "terminal" object of width `w` chars, and height `h` chars.
+t_terminal *create_terminal(int w, int h)
 {
     // get a pointer to some allocated memory for our char buffer
     char *mem = malloc(w * h * sizeof(char));
@@ -14,6 +40,106 @@ t_terminal* create_terminal(int w, int h)
 
     term->w = w;
     term->h = h;
-    term->terminal_mem = mem;
+    term->buf = mem;
+    // init cursor to 0
+    term->x = 0;
+    term->y = 0;
+}
 
+// free all memory used by terminal
+void terminal_destroy(t_terminal *t)
+{
+    // free the memory allocated to the terminal buffer
+    free(t->buf);
+    t->buf = NULL;
+    // free the memory used by the struct itself
+    free(t);
+    t = NULL;
+}
+
+// zero the terminal char buffer
+void terminal_clear(t_terminal *t)
+{
+    int buflen = t->w * t->h;
+    for (int i = 0; i < buflen; i++)
+    {
+        t->buf[i] = 0;
+    }
+}
+
+// Increase the cursor x, wrapping onto the next line if necessary
+void terminal_advance_cursor(t_terminal *t)
+{
+    t->x = t->x + 1; // increase the cursor x
+    if (t->x >= t->w)
+    {                    // if x overflow (max = w-1)
+        t->x = 0;        // set x to 0
+        t->y = t->y + 1; // inc cursor y
+        if (t->y >= t->h)
+        {             // if y overflow (max = h-1)
+            t->y = 0; // set y to 0
+        }
+    }
+}
+
+// Set the cursor position (clamp at w, h)
+void terminal_set_cursor(t_terminal *t, unsigned int x, unsigned int y){
+    // modulo operator without hardware divide likely to be expensive!
+    t->x = x % t->w;
+    t->y = y % t->h;
+    // alternative:
+    // t->x = x;
+    // t->y = y;
+    // clamp_cursor_x(t);
+    // clamp_cursor_y(t);
+}
+
+// write a byte at the current cursor position (optional call to terminal_advance_cursor)
+void terminal_write_char(t_terminal *t, char c, char auto_adv)
+{
+    int index = (t->y * t->w) + t->x;
+    t->buf[index] = c;
+    if (auto_adv)
+    {
+        terminal_advance_cursor(t);
+    }
+}
+
+// write a string to the terminal
+void terminal_write_raw_string(t_terminal *t, char *s)
+{
+    char c;
+    do
+    {
+        c = *s; // character of string (contents of s mem)
+        terminal_write_char(t, c, 1);  // print this char
+        s++;                           // increment pointer to move through array
+    } while (c != '\0');
+}
+
+// write a string to the terminal (interpret \r and \n as control chars)
+void terminal_write_string(t_terminal *t, char *s)
+{
+    char c;
+    do
+    {
+        c = *s; // character of string (contents of s mem)
+        switch (c)
+        {
+        case '\n':
+            unsigned int y = (t->y + 1) % t->h; // x=0, increment y
+            terminal_set_cursor(t, 0, y);
+            break;
+        case '\r':
+            terminal_set_cursor(t, 0, t->y);    // x=0
+            break;
+        case '\t':
+            // TODO: implement tab stops
+            // for now, fall through to `default`
+        default:
+            terminal_write_char(t, c, 1);  // print this char
+            break;
+        }
+        s++;                           // increment pointer to move through array
+    } while (c != '\0');
 }
--- software/src/terminal.h
+++ software/src/terminal.h
@@ -1,14 +1,52 @@
 #ifndef _TERMINAL_H_
 #define _TERMINAL_H_
+/*
+ *  Terminal.h
+ *  A reusable front-end for a text-based display, supporting
+ *  multiple display hardware types (such as the SSD1306 OLED
+ *  or a VGA screen)
+ */
+
+// This would probably be easier in C++!
 
 // declare a new struct type
 typedef struct
 {
-    int w;     // width
-    int h;     // height;
-    char *terminal_mem;
+    unsigned int w;     // width     (x_max = w - 1)
+    unsigned int h;     // height    (y_max = h - 1)
+    char *buf;
+    unsigned int x;      // cursor x
+    unsigned int y;      // cursor y
 } t_terminal;
 
-t_terminal *create_terminal(int w, int h);
+/*
+ * TERMINAL FUNCTIONS
+ */
+
+// allocate memory for a new "terminal" object of width `w` chars, and
+// height `h` chars.
+t_terminal* terminal_create(unsigned int w, unsigned int h);
+// free all memory used by terminal
+void terminal_destroy(t_terminal *t);
+// zero the terminal char buffer
+void terminal_clear(t_terminal *t);
+
+/*
+ * CURSOR FUNCTIONS
+ */
+// Increase the cursor x, wrapping onto the next line
+void terminal_advance_cursor(t_terminal *t);
+// Set the cursor position (clamp at w, h)
+void terminal_set_cursor(t_terminal *t, unsigned int x, unsigned int y);
+
+
+
+// write a byte at the current cursor position (optional call to terminal_advance_cursor)
+void terminal_write_char(t_terminal *t, char c, char auto_adv);
+
+// write a string to the terminal
+void terminal_write_raw_string(t_terminal *t, char *s);
+// write a string to the terminal (interpret \r and \n as control chars)
+void terminal_write_string(t_terminal *t, char *s);
 
 #endif // _TERMINAL_H_
