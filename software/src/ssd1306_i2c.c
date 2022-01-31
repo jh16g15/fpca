e796a8719aad5f49531f52644f6917b49f118bb3
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-01-31 23:31:19 +0000

    added ssd1306_refresh to write the contents of a t_terminal buffer to the display

--- software/src/ssd1306_i2c.c
+++ software/src/ssd1306_i2c.c
@@ -15,21 +15,20 @@
 #define SSD1306_HEIGHT_CHARS 4
 
 // only used for deprecated funcs
-#define CURSOR_MAX_X (16-1)
-#define CURSOR_MAX_Y (4-1)
+#define CURSOR_MAX_X (16 - 1)
+#define CURSOR_MAX_Y (4 - 1)
 
 #define SSD1306_ADDR_WR 0x78
 #define SSD1306_ADDR_RD 0x79
 
 // #define SSD1306_CONTROL_COMMAND 0x80            // one command byte follows
-#define SSD1306_CONTROL_COMMAND 0x00            // Continuous command bytes follow
-#define SSD1306_CONTROL_DATA 0xC0               // one data byte follows
-#define SSD1306_CONTROL_DATA_CONTINUOUS 0x40    //  Continuous data follows
+#define SSD1306_CONTROL_COMMAND 0x00         // Continuous command bytes follow
+#define SSD1306_CONTROL_DATA 0xC0            // one data byte follows
+#define SSD1306_CONTROL_DATA_CONTINUOUS 0x40 //  Continuous data follows
 
-
-#define SSD1306_ADDR_MODE_HORIZONTAL 0x0    // autoincrement to next page
+#define SSD1306_ADDR_MODE_HORIZONTAL 0x0 // autoincrement to next page
 #define SSD1306_ADDR_MODE_VERTICAL 0x1
-#define SSD1306_ADDR_MODE_PAGE 0x2          // wrap around to start of same page
+#define SSD1306_ADDR_MODE_PAGE 0x2 // wrap around to start of same page
 
 /*  This should give us a delay suitable for use in 100KHz I2C
     100KHz I2C will use a period of 50,000ns
@@ -55,8 +54,10 @@
 // 2 NOPs for ~108KHz (peak 170KHz)
 // 1 NOPs for ~114KHz (peak 181KHz)
 // quarter I2C clock period
-void i2c_delay(void){
-    for (int i = I2C_DELAY_LOOP_COUNT; i>0; i--){
+void i2c_delay(void)
+{
+    for (int i = I2C_DELAY_LOOP_COUNT; i > 0; i--)
+    {
         asm volatile( // 5 cycles each
             "NOP;"
             // "NOP;"
@@ -64,7 +65,7 @@ void i2c_delay(void){
 
         );
     }
-        /* -O0 loop maintenance:
+    /* -O0 loop maintenance:
         LW i                //  9 cycles
         ADDI i, -1          //  5 cycles
         SW i                //  8
@@ -80,10 +81,10 @@ void i2c_delay(void){
         For 400KHz, we need a delay of
 
         */
-
 }
 
-void i2c_start(void){
+void i2c_start(void)
+{
     I2C_SCL = 1;
     I2C_SDA = 1;
     // full clock cycle delay
@@ -99,11 +100,13 @@ void i2c_start(void){
     i2c_delay();
 }
 
-void i2c_write_byte(char data){
-    char bit;   // transmit MSB first
+void i2c_write_byte(char data)
+{
+    char bit; // transmit MSB first
 
     I2C_SCL = 0;
-    for (int i = 7; i > -1; i--){   // i = 7,6,5...1,0
+    for (int i = 7; i > -1; i--)
+    { // i = 7,6,5...1,0
         i2c_delay();
         // now set SDA
         I2C_SDA = get_bit_char(data, i);
@@ -127,11 +130,12 @@ void i2c_write_byte(char data){
     i2c_delay();
     I2C_SCL = 0;
     i2c_delay();
-    I2C_SDA = 0;    // reset SDA to 0 ready for STOP condition
+    I2C_SDA = 0; // reset SDA to 0 ready for STOP condition
     i2c_delay();
 }
 
-void i2c_stop(void){
+void i2c_stop(void)
+{
     I2C_SDA = 0;
     I2C_SCL = 1;
     // full clock cycle delay
@@ -147,7 +151,8 @@ void i2c_stop(void){
     i2c_delay();
 }
 
-void ssd1306_whole_display_on(void){
+void ssd1306_whole_display_on(void)
+{
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
@@ -155,7 +160,8 @@ void ssd1306_whole_display_on(void){
     i2c_stop();
 }
 
-void ssd1306_resume_ram_content(void){
+void ssd1306_resume_ram_content(void)
+{
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
@@ -163,7 +169,8 @@ void ssd1306_resume_ram_content(void){
     i2c_stop();
 }
 
-void ssd1306_display_init(void){
+void ssd1306_display_init(void)
+{
     // Charge pump setting -> Enable Charge Pump
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
@@ -182,7 +189,8 @@ void ssd1306_display_init(void){
     ssd1306_set_address_mode(SSD1306_ADDR_MODE_HORIZONTAL);
 }
 
-void ssd1306_display_sleep(void){
+void ssd1306_display_sleep(void)
+{
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
@@ -190,13 +198,12 @@ void ssd1306_display_sleep(void){
     i2c_stop();
 }
 
-
 void ssd1306_set_address_mode(char mode)
 {
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x20);   // set memory addressing mode
+    i2c_write_byte(0x20); // set memory addressing mode
     i2c_write_byte(mode);
     i2c_stop();
 }
@@ -229,7 +236,7 @@ void ssd1306_set_page_start_end(char start_page, char end_page)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x22);   // setup page start and end address
+    i2c_write_byte(0x22); // setup page start and end address
     i2c_write_byte(start_page);
     i2c_write_byte(end_page);
     i2c_stop();
@@ -239,14 +246,11 @@ void ssd1306_set_col_start_end(char start_col, char end_col)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x21);   // setup column start and end address
+    i2c_write_byte(0x21); // setup column start and end address
     i2c_write_byte(start_col);
     i2c_write_byte(end_col);
     i2c_stop();
 }
-
-
-
 
 void ssd1306_write_solid_char(void)
 {
@@ -254,7 +258,7 @@ void ssd1306_write_solid_char(void)
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_DATA_CONTINUOUS);
     // two `page` rows of 8 columns, so 16 bytes for a glyph
-    for (int i = 0; i < 16;i++)
+    for (int i = 0; i < 16; i++)
     {
         i2c_write_byte(0xff);
     }
@@ -270,7 +274,7 @@ void ssd1306_write_glyph(char id)
         // need to write one byte at a time otherwise we
         // can get dropped bytes (bitbanged I2C controller
         // not very good, and doesn't support clock stretching etc )
-        ssd1306_write_gram_byte(font_data[index+i]);
+        ssd1306_write_gram_byte(font_data[index + i]);
     }
 }
 
@@ -280,7 +284,7 @@ void ssd1306_clear_screen(void)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x21);   // setup column start and end address
+    i2c_write_byte(0x21); // setup column start and end address
     i2c_write_byte(0);
     i2c_write_byte(127);
     i2c_stop();
@@ -288,7 +292,7 @@ void ssd1306_clear_screen(void)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x22);   // setup page start and end address
+    i2c_write_byte(0x22); // setup page start and end address
     i2c_write_byte(0);
     i2c_write_byte(7);
     i2c_stop();
@@ -297,7 +301,8 @@ void ssd1306_clear_screen(void)
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_DATA_CONTINUOUS);
     int total_bytes = 128 * 8;
-    for (int i = 0; i < total_bytes; i++){
+    for (int i = 0; i < total_bytes; i++)
+    {
         i2c_write_byte(0x00);
     }
     i2c_stop();
@@ -309,7 +314,7 @@ void ssd1306_fill_screen(char d)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x21);   // setup column start and end address
+    i2c_write_byte(0x21); // setup column start and end address
     i2c_write_byte(0);
     i2c_write_byte(127);
     i2c_stop();
@@ -317,7 +322,7 @@ void ssd1306_fill_screen(char d)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_COMMAND);
-    i2c_write_byte(0x22);   // setup page start and end address
+    i2c_write_byte(0x22); // setup page start and end address
     i2c_write_byte(0);
     i2c_write_byte(7);
     i2c_stop();
@@ -326,7 +331,8 @@ void ssd1306_fill_screen(char d)
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_DATA_CONTINUOUS);
     int total_bytes = 128 * 8;
-    for (int i = 0; i < total_bytes; i++){
+    for (int i = 0; i < total_bytes; i++)
+    {
         i2c_write_byte(d);
     }
     i2c_stop();
@@ -347,7 +353,8 @@ void ssd1306_write_gram_bytes(char d, char num)
     i2c_start();
     i2c_write_byte(SSD1306_ADDR_WR);
     i2c_write_byte(SSD1306_CONTROL_DATA_CONTINUOUS);
-    for (int i = 0; i < num; i++){
+    for (int i = 0; i < num; i++)
+    {
         // i2c_write_byte(d[i]);
         i2c_write_byte(d);
     }
@@ -359,11 +366,13 @@ void ssd1306_advance_cursor(char *x_ptr, char *y_ptr)
 {
     uart_puts("Advancing Cursor");
     *x_ptr = *x_ptr + 1;
-    if (*x_ptr > CURSOR_MAX_X){ // handle X wrapping
+    if (*x_ptr > CURSOR_MAX_X)
+    { // handle X wrapping
         uart_puts("Handling cursor x overflow");
         *x_ptr = 0;
         *y_ptr = *y_ptr + 1;
-        if (*y_ptr > CURSOR_MAX_Y){ // handle Y wrapping
+        if (*y_ptr > CURSOR_MAX_Y)
+        { // handle Y wrapping
             uart_puts("Handling cursor y overflow");
             *y_ptr = 0;
         }
@@ -385,17 +394,17 @@ void ssd1306_puts(char *s, char *x_ptr, char *y_ptr)
     char c;
     do
     {
-        c = *s;  // character of string (contents of s mem)
+        c = *s; // character of string (contents of s mem)
         // we could optionally check for /r and /n here
         ssd1306_putc(c, x_ptr, y_ptr); // print this char
-        s++;     // increment pointer to move through array
+        s++;                           // increment pointer to move through array
     } while (c != '\0');
 }
 
 void ssd1306_newline(char *x_ptr, char *y_ptr)
 {
-    *x_ptr = 0; // move X to start of  line
-    char new_y = *y_ptr;    // increment y
+    *x_ptr = 0;          // move X to start of  line
+    char new_y = *y_ptr; // increment y
     new_y = (new_y + 1) % (CURSOR_MAX_Y + 1);
     *y_ptr = new_y;
     ssd1306_set_cursor(0, new_y);
@@ -411,11 +420,12 @@ void ssd1306_carriage_return(char *x_ptr, char *y_ptr)
 void ssd1306_clearline(char *x_ptr, char *y_ptr)
 {
     ssd1306_carriage_return(x_ptr, y_ptr);
-    for (int i = 0; i <= CURSOR_MAX_X; i++){
+    for (int i = 0; i <= CURSOR_MAX_X; i++)
+    {
         ssd1306_write_glyph(0);
         ssd1306_advance_cursor(x_ptr, y_ptr);
     }
-    ssd1306_carriage_return(x_ptr, y_ptr);  // this might put us on the next line
+    ssd1306_carriage_return(x_ptr, y_ptr); // this might put us on the next line
 }
 
 /*
@@ -428,10 +438,34 @@ void ssd1306_refresh(t_terminal *t)
 
     // iterate through t->buf until screen is full
     int ssd1306_numchars = SSD1306_HEIGHT_CHARS * SSD1306_WIDTH_CHARS;
+    int char_id;
+    int char_base;
+    // for each row of text
+    for (int row = 0; row < SSD1306_HEIGHT_CHARS; row++)
+    {
+        // write the top row of bytes first
+        for (int col = 0; col < SSD1306_WIDTH_CHARS; col++)
+        {
+            // get the character in that position in the terminal
+            char_id = t->buf[(row * SSD1306_WIDTH_CHARS) + col];
+            char_base = char_id * 16;
+            for (int i = 0; i < 8; i++)
+            {
+                ssd1306_write_gram_byte(font_data[char_base + i]);
+            }
+        }
 
-    // write the top row of bytes first
-
-    // index into font rom
-
+        // then the second row of bytes
+        for (int col = 0; col < SSD1306_WIDTH_CHARS; col++)
+        {
+            // get the character in that position in the terminal
+            char_id = t->buf[(row * SSD1306_WIDTH_CHARS) + col];
+            char_base = char_id * 16 + 8;
+            for (int i = 0; i < 8; i++)
+            {
+                ssd1306_write_gram_byte(font_data[char_base + i]);
+            }
+        }
+    }
 
 }
