0c91c25ee4f8661ed3821759ab0185861e3c1258
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-02-01 00:07:33 +0000

    initial testing of Terminal code

--- software/src/main.c
+++ software/src/main.c
@@ -36,14 +36,57 @@ void main(void)
     Q_SSEG = 0xc0de;
     uart_set_baud(9600);
 
-    t_terminal *oled_term = create_terminal(16, 4);
+    t_terminal *oled_term = terminal_create(16, 4);
 
     ssd1306_display_sleep();
     // Enable the charge pump and turn the display on
     ssd1306_display_init();
+    ssd1306_whole_display_on();
+    ssd1306_resume_ram_content();
     ssd1306_clear_screen();
 
-    //*     SSD1306 Font Test
+    //*     Terminal Test
+    terminal_clear(oled_term);
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "hello\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "This is the FPCA\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "Loading\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading.\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading..\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading...\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading....\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading.....\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading......\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading.......\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading........\n");
+    ssd1306_refresh(oled_term);
+    terminal_write_string(oled_term, "\rLoading.........\n");
+    ssd1306_refresh(oled_term);
+    while(1){}
+
+
+    // int tmp=0;
+    // while (1)
+    // {
+    //     Q_SSEG = tmp;
+    //     terminal_write_char(oled_term, tmp, 1);
+    //     ssd1306_refresh(oled_term);
+    //     tmp++;
+    // }
+
+    //*/
+
+    /*     SSD1306 Font Test
     char x = 0;
     char y = 0;
     ssd1306_set_cursor(x, y);
@@ -78,7 +121,7 @@ void main(void)
     while (1)
     {
     }
-
+    //*/
 
     uart_puts("ERR: Should never get here!");
 }
