65c89836645ba3b30f42ae295320c57be7fa5d4f
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-04-24 21:13:04 +0100

    Update run.py to allow simulation of GOWIN primitives with GHDL

--- hdl/sim/run.py
+++ hdl/sim/run.py
@@ -20,7 +20,9 @@ import os
 VU = VUnit.from_argv()
 VU.add_verification_components()
 
-VU.add_compile_option("ghdl.a_flags", ["--ieee=standard", "--std=08", "-frelaxed-rules"])
+USE_GOWIN_SIMLIB = True
+gowin_simlib = "/mnt/c/Gowin/Gowin_V1.9.8.03_Education/IDE/simlib/gw1n/prim_sim.vhd"
+
 
 ## need to use "resolve()" to get abspath
 # print(f"0:{os.listdir(Path(__file__).resolve().parents[0])}")
@@ -29,6 +31,8 @@ VU.add_compile_option("ghdl.a_flags", ["--ieee=standard", "--std=08", "-frelaxed
 
 sim_dir = Path(__file__).parent
 src_dir = Path(__file__).resolve().parents[1] / "src"
+
+
 
 VU.add_library("lib")
 VU.add_source_files(sim_dir / "tb" / "*.vhd", "lib")
@@ -44,7 +48,14 @@ VU.add_source_files(src_dir / "peripherals" / "common" / "*.vhd", "lib", allow_e
 VU.add_source_files(src_dir / "peripherals" / "uart" / "*.vhd", "lib", allow_empty=True)
 VU.add_source_files(src_dir / "peripherals" / "timer" / "*.vhd", "lib", allow_empty=True)
 
-
+# The GOWIN simulation primitives require the dodgy Synopsys std_logic_arith,
+# std_logic_signed and std_logic_unsigned packages, so only include them if necessary
+if USE_GOWIN_SIMLIB:
+    VU.add_source_files(gowin_simlib, "lib")
+    VU.add_compile_option("ghdl.a_flags", ["--ieee=standard", "-fsynopsys", "--std=08", "-frelaxed-rules"])
+    VU.set_sim_option("ghdl.elab_flags", ["-fsynopsys"])
+else:
+    VU.add_compile_option("ghdl.a_flags", ["--ieee=standard", "--std=08", "-frelaxed-rules"])
 
 
 VU.main()
