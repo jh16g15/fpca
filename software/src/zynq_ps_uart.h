b369d901eff8ef6b088b14e87ec78c66975ef557
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2022-09-15 01:12:57 +0100

    Initial commit of Zynq PS UART C driver for FPCA CPU

---
+++ software/src/zynq_ps_uart.h
@@ -0,0 +1,47 @@
+
+#ifndef _ZYNQ_PS_UART_H
+#define _ZYNQ_PS_UART_H
+
+#include "cpu.h"
+
+#ifdef PYNQ_Z2  // only valid code if running on a Zynq device
+
+// Registers
+#define ZYNQ_PS_UART_CR (*((volatile unsigned long *)0xE0000000))
+#define ZYNQ_PS_UART_BAUDGEN (*((volatile unsigned long *)0xE0000018))
+#define ZYNQ_PS_UART_FIFO (*((volatile unsigned char *)0xE0000030)) // transmit and receive
+
+
+void zynq_ps_uart_setup(){
+    // ZYNQ_PS_UART_BAUDGEN = 0x00000364;  // set baud rate to 115200
+    ZYNQ_PS_UART_CR = 0x00000117;       // Enable Tx/Rx, soft reset FIFOs
+}
+
+// void zynq_ps_uart_set_baud(){
+
+// }
+
+void zynq_ps_uart_putc(char c){
+    ZYNQ_PS_UART_FIFO = c;
+}
+
+void zynq_ps_uart_puts(char *s)
+{
+    char c;
+    do
+    {
+        c = *s;  // character of string (contents of s mem)
+        zynq_ps_uart_putc(c); // print this char
+        s++;     // increment pointer to move through array
+    } while (c != '\0');
+    zynq_ps_uart_putc('\r');    // for PUTTY compat
+    zynq_ps_uart_putc('\n');
+}
+
+char zynq_ps_uart_getc(){
+    return ZYNQ_PS_UART_FIFO;
+}
+
+#endif // PYNQ_Z2
+
+#endif  //_ZYNQ_PS_UART_H
