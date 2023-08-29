0810ff358b917e1bd5745fe9b7e4f0f34c1ed49e
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2023-08-29 18:59:45 +0100

    cleanup of unused code

--- software/src/main.c
+++ software/src/main.c
@@ -82,28 +82,18 @@ void main(void)
 
     Q_SSEG = 0x0;
     // pointer to initial terminal created on the stack, containing the static address of the terminal
-    t_terminal *term = console_init();
+    console_init();
     cls();
 
     printf_("Hello World\n");
-    printf_("Initial Terminal Address : %p\n", term);
-    printf_("CPU Frequency = %i MHz\n", GPIO_SOC_FREQ/1000000);
-    printf_("CPU Memory    = %i KB\n", GPIO_SOC_MEM/1024);
-
-    printf_("Terminal Address       : %p\n", term);
-    printf_("Terminal Width Addr    : %p\n", &term->w);
-    printf_("Terminal Height Addr   : %p\n", &term->h);
-    printf_("Terminal Buf Addr      : %p\n", &term->buf);
-    printf_("Terminal X   Addr      : %p\n", &term->x);
-    printf_("Terminal Y Addr        : %p\n", &term->y);
-    printf_("Terminal top line Adr  : %p\n", &term->line_at_top);
-    printf_("Terminal Buf contents start : %p\n", term->buf);
-
+    printf_("Console is working!\n");
+    printf_("CPU Arch      : %s\n", "RISC-V RV32I");
+    printf_("CPU Frequency : %i MHz\n", GPIO_SOC_FREQ/1000000);
+    printf_("CPU Memory    : %i KB\n", GPIO_SOC_MEM/1024);
 
     int tmp = 21;
     int test_var = 0;
-    printf_("console is working!\n");
-    cls();
+
     printf_("Happy days! Test Var location (on stack): %p\n", &test_var);
     putchar_(1);
     putchar_(' ');
@@ -119,36 +109,4 @@ void main(void)
     }
 
 
-    // u8 linebuf[80];
-    // u32 count = 0;
-    // terminal_clear(t);
-    // text_refresh_from_terminal(t);
-    // terminal_write_string(t, "Jello\n");
-
-    // terminal_write_string(t, "=======================================\n");
-    // terminal_write_string(t, "Friendly Programmable Computing Asset  \n");
-    // terminal_write_string(t, "=======================================\n");
-    // terminal_write_string(t, "Architecture: RISC-V RV32I             \n");
-    // terminal_write_string(t, "Frequency: 25MHz                       \n");
-    // terminal_write_string(t, "Memory: 16KB                           \n");
-    // terminal_write_string(t, "Press the UP button to continue\n");
-
-    // text_refresh_from_terminal(t);
-    // wait_for_btn_press(BTN_U);
-
-    // while(1){
-    //     // add line number at start
-    //     u32_to_hstring(count, linebuf, 80);
-    //     terminal_write_string(t, "\n"); // we get a bit more on the screen if we have the \n at the start
-    //     terminal_write_string(t, linebuf);
-    //     count++;
-
-    //     terminal_write_string(t, " This is a string");
-
-    //     Q_SSEG = 0x1000;
-    //     wait_for_btn_press(BTN_L);
-    //     text_refresh_from_terminal(t);
-    //     Q_SSEG = 0x0001;
-    //     // wait_for_btn_press(BTN_R);
-    // }
 }
